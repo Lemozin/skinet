58d79abbd41eb5bb77a73c574dae1d83ce2f5e32
LeonardM <LeonardM@hollywoodbets.net>
2023-06-17 12:56:07 +0200

    End of section 5

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -6,6 +6,10 @@ namespace API.Helpers
 {
     public class MappingProfiles : Profile
     {
+        /// <summary>
+        /// Creates a mapping configuration from TSource type to TDestination type
+        /// </summary>
+        /// <returns>Mapping expression for more configuration options</returns>
         public MappingProfiles()
         {
             CreateMap<Product, ProductToReturnDto>()
